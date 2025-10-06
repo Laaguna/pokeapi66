cf355bad866641fab71d48d18adfff1375e72648
Camilo Mahecha <camimahe.12@gmail.com>
2025-10-06 10:32:34 -0500

    Update profile_page.dart

--- lib/ui/pages/profile/profile_page.dart
+++ lib/ui/pages/profile/profile_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../../../domain/models/pokemon/pokemon.dart';
+import '../../widgets/error_widget.dart';
 
 class ProfilePage extends StatelessWidget {
 
@@ -14,7 +15,12 @@ class ProfilePage extends StatelessWidget {
         title: const Text('Profile'),
       ),
       body: const  Center(
-        child: const Text('User Profile'),
+        child: ErrorStateWidget(
+          canRetry: false,
+          imagePath: 'assets/images/coming_soon.png',
+          title: '¡Muy pronto disponible!',
+          description: 'Estamos trabajando para traerte esta sección. Vuelve más adelante para descubrir todas las novedades.',
+        ),
       ),
     );
   }
