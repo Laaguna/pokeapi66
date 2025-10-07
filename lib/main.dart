34e21577a62fc9518f1c5e8924fc2319a622a33b
Camilo Mahecha <camimahe.12@gmail.com>
2025-10-07 12:07:48 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.red,
+          seedColor: Colors.blueAccent,
           brightness: Brightness.light,
         ),
         useMaterial3: true,
